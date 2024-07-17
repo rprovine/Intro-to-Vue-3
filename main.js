958de9e4bb4baee1be1529c958127e650abb5d0c
rprovine <rprovine@gmail.com>
2024-07-16 15:55:47 -1000

    beginning

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description: "A pair of warm, fuzzy socks",
+      image: "/assets/images/socks_green.jpg",
+      url: "https://www.google.com",
+    };
+  },
+});
+
+app.mount("#app");
