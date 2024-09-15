4e117546eb7a82cbcfbcb15013ab1cd5fd14514c
rprovine <rprovine@gmail.com>
2024-09-14 20:15:35 -1000

    push

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       description: "A pair of warm, fuzzy socks",
-      image: "/assets/images/socks_green.jpg",
+      image: "/images/socks_green.jpg",
       url: "https://www.google.com",
     };
   },
